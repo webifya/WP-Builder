eaf3f188e6a846c3f16c3f9c7c7dc72dae964123
webifya <161525559+webifya@users.noreply.github.com>
2026-07-29 04:12:38 +0600

    Complete visual editor milestone 0.2

--- src/Rendering/Renderer.php
+++ src/Rendering/Renderer.php
@@ -23,7 +23,10 @@ final class Renderer implements Service {
 			return $content;
 		}
 		wp_enqueue_style( 'wp-builder' );
-		return '<div class="wpb-page">' . $this->elements( (array) ( $document['elements'] ?? array() ) ) . '</div>';
+		$elements = (array) ( $document['elements'] ?? array() );
+		$css      = $this->responsive_css( $elements );
+		return ( $css ? '<style id="wpb-responsive-' . absint( $post_id ) . '">' . $css . '</style>' : '' )
+			. '<div class="wpb-page">' . $this->elements( $elements ) . '</div>';
 	}
 
 	private function elements( array $elements ): string {
@@ -32,20 +35,62 @@ final class Renderer implements Service {
 			$type     = sanitize_key( $element['type'] ?? '' );
 			$props    = (array) ( $element['props'] ?? array() );
 			$children = $this->elements( (array) ( $element['children'] ?? array() ) );
-			$class    = 'wpb-element wpb-' . $type;
+			$props['text'] = $props['text'] ?? '';
+			$id            = preg_replace( '/[^a-zA-Z0-9_-]/', '', (string) ( $element['id'] ?? '' ) );
+			$class         = 'wpb-element wpb-' . $type . ( $id ? ' wpb-id-' . $id : '' );
+			foreach ( array( 'desktop', 'tablet', 'mobile' ) as $device ) {
+				if ( ! empty( $props[ 'hide_' . $device ] ) ) {
+					$class .= ' wpb-hide-' . $device;
+				}
+			}
+			$style = $this->style_attribute( (array) ( $element['styles']['desktop'] ?? array() ) );
 			switch ( $type ) {
 				case 'heading':
 					$tag   = in_array( $props['tag'] ?? 'h2', array( 'h1', 'h2', 'h3', 'h4', 'h5', 'h6' ), true ) ? $props['tag'] : 'h2';
-					$html .= sprintf( '<%1$s class="%2$s">%3$s</%1$s>', $tag, esc_attr( $class ), wp_kses_post( $props['text'] ?? '' ) );
+					$html .= sprintf( '<%1$s class="%2$s"%3$s>%4$s</%1$s>', $tag, esc_attr( $class ), $style, wp_kses_post( $props['text'] ) );
 					break;
 				case 'text':
-					$html .= '<div class="' . esc_attr( $class ) . '">' . wp_kses_post( $props['text'] ?? '' ) . '</div>';
+					$html .= '<div class="' . esc_attr( $class ) . '"' . $style . '>' . wp_kses_post( $props['text'] ) . '</div>';
 					break;
 				case 'image':
 					$html .= wp_get_attachment_image( absint( $props['attachmentId'] ?? 0 ), 'full', false, array( 'class' => $class ) );
 					break;
 				case 'button':
-					$html .= sprintf( '<a class="%s" href="%s">%s</a>', esc_attr( $class ), esc_url( $props['url'] ?? '#' ), esc_html( $props['text'] ?? __( 'Button', 'wp-builder' ) ) );
+					$html .= sprintf( '<a class="%s" href="%s"%s>%s</a>', esc_attr( $class ), esc_url( $props['url'] ?? '#' ), $style, esc_html( $props['text'] ?: __( 'Button', 'wp-builder' ) ) );
+					break;
+				case 'video':
+					$html .= wp_video_shortcode( array( 'src' => esc_url_raw( $props['url'] ?? '' ) ) );
+					break;
+				case 'audio':
+					$html .= wp_audio_shortcode( array( 'src' => esc_url_raw( $props['url'] ?? '' ) ) );
+					break;
+				case 'list':
+					$items = preg_split( '/\r\n|\r|\n/', wp_strip_all_tags( $props['text'] ) );
+					$html .= '<ul class="' . esc_attr( $class ) . '"' . $style . '>';
+					foreach ( $items as $item ) {
+						if ( '' !== trim( $item ) ) {
+							$html .= '<li>' . esc_html( trim( $item ) ) . '</li>';
+						}
+					}
+					$html .= '</ul>';
+					break;
+				case 'divider':
+					$html .= '<hr class="' . esc_attr( $class ) . '"' . $style . '>';
+					break;
+				case 'spacer':
+					$html .= '<div class="' . esc_attr( $class ) . '" aria-hidden="true"' . $style . '></div>';
+					break;
+				case 'alert':
+					$html .= '<div class="' . esc_attr( $class ) . '" role="status"' . $style . '>' . esc_html( $props['text'] ) . '</div>';
+					break;
+				case 'progress':
+					$value = min( 100, max( 0, absint( $props['value'] ?? 0 ) ) );
+					$html .= sprintf( '<progress class="%s" max="100" value="%d"%s>%d%%</progress>', esc_attr( $class ), $value, $style, $value );
+					break;
+				case 'counter':
+				case 'rating':
+				case 'icon':
+					$html .= '<div class="' . esc_attr( $class ) . '"' . $style . '>' . esc_html( $props['text'] ) . '</div>';
 					break;
 				case 'shortcode':
 					$html .= '<div class="' . esc_attr( $class ) . '">' . do_shortcode( sanitize_text_field( $props['code'] ?? '' ) ) . '</div>';
@@ -54,9 +99,64 @@ final class Renderer implements Service {
 					$html .= '<div class="' . esc_attr( $class ) . '">' . wp_kses_post( $props['html'] ?? '' ) . '</div>';
 					break;
 				default:
-					$html .= '<div class="' . esc_attr( $class ) . '">' . $children . '</div>';
+					$html .= '<div class="' . esc_attr( $class ) . '"' . $style . '>' . ( $children ?: wp_kses_post( $props['text'] ) ) . '</div>';
 			}
 		}
 		return $html;
+	}
+
+	private function style_attribute( array $rules ): string {
+		$map = array(
+			'margin'         => 'margin',
+			'padding'        => 'padding',
+			'color'          => 'color',
+			'backgroundColor'=> 'background-color',
+			'borderColor'    => 'border-color',
+			'borderWidth'    => 'border-width',
+			'borderRadius'   => 'border-radius',
+			'fontSize'       => 'font-size',
+			'fontWeight'     => 'font-weight',
+			'textAlign'      => 'text-align',
+			'width'          => 'width',
+			'minHeight'      => 'min-height',
+			'display'        => 'display',
+			'flexDirection'  => 'flex-direction',
+			'justifyContent' => 'justify-content',
+			'alignItems'     => 'align-items',
+			'gap'            => 'gap',
+		);
+		$css = '';
+		foreach ( $rules as $property => $value ) {
+			if ( isset( $map[ $property ] ) && is_string( $value ) && ! preg_match( '/[;{}]|url\s*\(/i', $value ) ) {
+				$css .= $map[ $property ] . ':' . $value . ';';
+			}
+		}
+		return $css ? ' style="' . esc_attr( $css ) . '"' : '';
+	}
+
+	private function responsive_css( array $elements ): string {
+		$tablet = '';
+		$mobile = '';
+		$this->collect_responsive_css( $elements, $tablet, $mobile );
+		$css = $tablet ? '@media(max-width:1024px){' . $tablet . '}' : '';
+		$css .= $mobile ? '@media(max-width:767px){' . $mobile . '}' : '';
+		return $css;
+	}
+
+	private function collect_responsive_css( array $elements, string &$tablet, string &$mobile ): void {
+		foreach ( $elements as $element ) {
+			$id = preg_replace( '/[^a-zA-Z0-9_-]/', '', (string) ( $element['id'] ?? '' ) );
+			if ( $id ) {
+				$tablet_rules = $this->style_attribute( (array) ( $element['styles']['tablet'] ?? array() ) );
+				$mobile_rules = $this->style_attribute( (array) ( $element['styles']['mobile'] ?? array() ) );
+				if ( $tablet_rules ) {
+					$tablet .= '.wpb-id-' . $id . '{' . substr( $tablet_rules, 8, -1 ) . '}';
+				}
+				if ( $mobile_rules ) {
+					$mobile .= '.wpb-id-' . $id . '{' . substr( $mobile_rules, 8, -1 ) . '}';
+				}
+			}
+			$this->collect_responsive_css( (array) ( $element['children'] ?? array() ), $tablet, $mobile );
+		}
 	}
 }
